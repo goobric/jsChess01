b665c00f699fdbe1ebef83a665780e55f16b530f
mlaane <goobric@gmail.com>
2023-12-02 18:19:34 +0100

    all

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // General App Configuration
+
+const gameBoard = document.querySelector('#gameboard');
+const playerDisplay = document.querySelector('#player');
+
+const infoDisplay = document.querySelector('#info-display');
+
+const width = 8;
+// pieces
+const startPieces = [];
